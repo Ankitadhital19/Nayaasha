ed6b1a98b543a50b45604c59d3270b452e53f793
Ankitadhital19 <ankidhital@gmail.com>
2024-09-29 13:40:49 +0545

    Fixed orphanage creation

--- app/graphql/types/orphanage/orphanage_type.rb
+++ app/graphql/types/orphanage/orphanage_type.rb
@@ -4,6 +4,7 @@ module Types
       field :id, ID, null: false
       field :name, String, null: false
       field :location, String, null: false
+      field :roles, Enums::OrphanageRolesEnum, null: true
       field :user_id, ID, null: false
       field :created_at, GraphQL::Types::ISO8601DateTime, null: false
       field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
